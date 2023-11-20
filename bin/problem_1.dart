95cf5dbf8bf33d015831c29a7bfbe8aea0b15df2
saiful <saiful0687@gmail.com>
2023-11-20 17:35:03 -0500

    update

--- bin/problem_1.dart
+++ bin/problem_1.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 void main(){
 
-var  celcius=0;
+var  celcius=1;
 print("Enter the temperature in Celsius");
-celcius=stdin.readLineSync() as int;
+//celcius=stdin.readLineSync() as int;
   var fehnerheit=(celcius*9/5)+32;
 
   print("The temperature in Fahrenheit is: $fehnerheit");
